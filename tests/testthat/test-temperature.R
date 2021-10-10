94c54d20c7fa8671fd0c12664b0c3ffc12fe6ae9
Fabian Mitze <FabianMitze@users.noreply.github.com>
2021-10-10 19:16:52 +0200

    corrected and renamed functions

--- tests/testthat/test-temperature.R
+++ tests/testthat/test-temperature.R
@@ -1,6 +1,8 @@
 context("Temperature functions")
 
 test_that("Potential temperature works", {
-  expect_equal(temp_pot_temp(25, 1000), 299.2745, tolerance=1e-3)
-  expect_equal(temp_pot_temp(c(25, NA), c(NA, 1000)), c(NA_integer_, NA_integer_))
+  expect_equal(temp_pot_temp(25, 1000),
+               26.124, tolerance = 1e-3)
+  expect_equal(temp_pot_temp(c(25, NA), c(NA, 1000)),
+               c(NA_integer_, NA_integer_))
 })
--- tests/testthat/test-utility_turbulent_flux.R
+++ tests/testthat/test-utility_turbulent_flux.R
@@ -1,17 +1,23 @@
 context("Utility Turbulent Flux")
 
-test_that("sc coefficient", {
-  expect_equal(sc(22), 0.001038809, tolerance = 1e-3)
+# test data
+t <- 22.53
+dpot <- -0.1254715
+dah <- -0.0001465109
+
+
+test_that("sc PT coefficient", {
+  expect_equal(sc(t = t), 0.00106759104, tolerance = 1e-3)
 })
 
-test_that("lambda coefficient", {
-  expect_equal(lamb(22), 0.0004000587, tolerance = 1e-3)
+test_that("gamma PT coefficient", {
+  expect_equal(gam(t = t), 0.0004001164196, tolerance = 1e-3)
 })
 
 test_that("heat capacity", {
-  expect_equal(heat_capacity(22.08298), 1177.396, tolerance = 1e-3)
+  expect_equal(heat_capacity(t = t), 1175.262181, tolerance = 1e-3)
 })
 
 test_that("Bowen Coefficient", {
-  expect_equal(bowen_ratio(22.08298, -0.1254715, -0.0001465109), 0.4118242, tolerance = 1e-3)
+  expect_equal(bowen_ratio(t = t, dpot = dpot, dah = dah), 0.4112514782, tolerance = 1e-3)
 })
