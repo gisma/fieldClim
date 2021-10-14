872995b8dd859054a54e625c734d5d8d3859e356
Fabian Mitze <FabianMitze@users.noreply.github.com>
2021-10-14 15:01:28 +0200

    undo albedo deletion

--- R/data.R
+++ R/data.R
@@ -9,24 +9,25 @@
 #' Surface type: "field"
 #' Ground texture: "clay"
 #'
-#' @format A data frame with 288 rows and 20 variables:
+#' @format A data frame with 288 rows and 21 variables:
 #' \describe{
 #'   \item{datetime}{Date and time of measurement in POSIXt-format.}
 #'   \item{t1}{Temperature in °C at 2m height (lower level).}
 #'   \item{hum1}{Humidity in % at 2m height (lower level).}
 #'   \item{t2}{Temperature in °C at 10m height (upper level).}
 #'   \item{hum2}{Humidity in % at 10m height (upper level).}
-#'   \item{rad_sw_in}{Incoming shortwave radiation in W/m^2.}
-#'   \item{rad_sw_out}{Outgoing shortwave radiation in W/m^2.}
-#'   \item{rad_lw_in}{Incoming longwave radiation in W/m^2.}
-#'   \item{rad_lw_out}{Outgoing longwave radiation in W/m^2.}
-#'   \item{rad_sw_bal}{Shortwave radiation balance in W/m^2.}
-#'   \item{rad_lw_bal}{Longwave radiation balance in W/m^2.}
-#'   \item{rad_bal}{Total radiation balance in W/m^2.}
+#'   \item{rad_sw_in}{Incoming shortwave radiation in W/m².}
+#'   \item{rad_sw_out}{Outgoing shortwave radiation in W/².}
+#'   \item{rad_lw_in}{Incoming longwave radiation in W/m².}
+#'   \item{rad_lw_out}{Outgoing longwave radiation in W/m².}
+#'   \item{rad_sw_bal}{Shortwave radiation balance in W/m².}
+#'   \item{rad_lw_bal}{Longwave radiation balance in W/m².}
+#'   \item{albedo}{Albedo of ground.}
+#'   \item{rad_bal}{Total radiation balance in W/m².}
 #'   \item{water_vol_soil}{Moisture of ground in %.}
 #'   \item{t_surface}{Surface temperature in °C.}
 #'   \item{ts1}{Temperature soil, 30 cm depth.}
-#'   \item{heatflux_soil}{Soil heat flux in W/m^2.}
+#'   \item{heatflux_soil}{Soil heat flux in W/m².}
 #'   \item{v1}{Wind velocity in m/s at 2m height (lower level).}
 #'   \item{v2}{Wind velocity in m/s at 10m height (upper level).}
 #'   \item{p1}{Air pressure in hPa (lower level).}
